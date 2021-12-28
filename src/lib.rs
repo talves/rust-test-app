1688e27ee9889edc1902a1e8d72758ae8dadd919
talves <tony@3alves.com>
2021-12-28 14:16:46 -0800

    modules: publid enums

--- src/lib.rs
+++ src/lib.rs
@@ -15,6 +15,10 @@ mod front_of_house {
 }
 
 pub mod back_of_house {
+    pub enum Appetizer {
+        Soup,
+        Salad,
+    }
     pub struct Breakfast {
         pub toast: String,
         seasonal_fruit: String,
@@ -31,6 +35,8 @@ pub mod back_of_house {
 }
 
 pub fn eat_at_restaurant() {
+    let order1 = back_of_house::Appetizer::Soup;
+    let order2 = back_of_house::Appetizer::Salad;
     // Absolute path
     crate::front_of_house::hosting::add_to_waitlist();
 
