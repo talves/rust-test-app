7095920105e20cd27a9764a90763c95f4b92575e
talves <tony@3alves.com>
2022-01-01 14:56:35 -0800

    error: ? is less code

--- src/main.rs
+++ src/main.rs
@@ -437,4 +437,9 @@ fn read_username_from_file() -> Result<String, io::Error> {
     // let mut s = String::new();
     // f.read_to_string(&mut s)?;
     // Ok(s)
+
+    // OR even shorter than that
+    // let mut s = String::new();
+    // File::open("hello.txt")?.read_to_string(&mut s)?;
+    // Ok(s)
 }
