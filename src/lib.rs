68f40e6ef9037d8d0fa1618cf1b0cdee7d1a4309
talves <tony@3alves.com>
2023-08-27 11:29:17 -0700

    comment: private function

--- src/lib.rs
+++ src/lib.rs
@@ -32,6 +32,8 @@ pub fn eat_at_restaurant() {
 
     // Relative path
     front_of_house::hosting::add_to_waitlist();
+    // this is a private function, not accessible
+    // front_of_house::hosting::seat_at_table();
 
     // Order a breakfast in the summer with Rye toast
     let mut meal = back_of_house::Breakfast::summer("Rye");
