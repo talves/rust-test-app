5c05aab137d00182bb09b6f22c3e86e593dfb464
talves <tony@3alves.com>
2023-08-27 12:19:25 -0700

    traits: function switch

--- src/main.rs
+++ src/main.rs
@@ -34,6 +34,8 @@ fn main() {
     traits();
     // lifetimes
     lifetimes();
+    // traits
+    dynamic_traits();
 }
 
 fn scope_example() {
@@ -522,26 +524,43 @@ fn returns_summarizable() -> impl Summary {
 }
 
 // Does not compile right now
-// fn returns_summarizable_switch(switch: bool) -> impl Summary {
-//     if switch {
-//         NewsArticle {
-//             headline: String::from("Penguins win the Stanley Cup Championship!"),
-//             location: String::from("Pittsburgh, PA, USA"),
-//             author: String::from("Iceburgh"),
-//             content: String::from(
-//                 "The Pittsburgh Penguins once again are the best \
-//                  hockey team in the NHL.",
-//             ),
-//         }
-//     } else {
-//         Tweet {
-//             username: String::from("horse_ebooks"),
-//             content: String::from("of course, as you probably already know, people"),
-//             reply: false,
-//             retweet: false,
-//         }
-//     }
-// }
+fn returns_summarizable_switch(switch: bool) -> Box<dyn Summary> {
+    if switch {
+        Box::new(NewsArticle {
+            headline: String::from("Penguins win the Stanley Cup Championship!"),
+            location: String::from("Pittsburgh, PA, USA"),
+            author: String::from("Iceburgh"),
+            content: String::from(
+                "The Pittsburgh Penguins once again are the best \
+                 hockey team in the NHL.",
+            ),
+        })
+    } else {
+        Box::new(Tweet {
+            username: String::from("horse_ebooks"),
+            content: String::from("of course, as you probably already know, people"),
+            reply: false,
+            retweet: false,
+        })
+    }
+}
+
+// Lifetimes
+
+fn dynamic_traits() {
+    let summary = returns_summarizable_switch(true);
+    println!(
+        "summary: {} {}",
+        summary.summarize_author(),
+        summary.summarize()
+    );
+    let summary = returns_summarizable_switch(false);
+    println!(
+        "summary: {} {}",
+        summary.summarize_author(),
+        summary.summarize()
+    );
+}
 
 // Lifetimes
 
